3011d1715245f6d4b764cf701a37ad315c5a2a1a
LucaNerlich <luca.nerlich@googlemail.com>
2015-04-25 01:54:33 +0200

    2

---
+++ src/Praktikum1/Main.java
@@ -0,0 +1,20 @@
+package Praktikum1;
+/**
+ * Praktikum RB, SS 2015
+ * Gruppe: Luca Nerlich (Lucasteffen.Nerlich@haw-hamburg.de)
+ * 		   Daniel Sommerlig (Daniel.Sommerlig@haw-hamburg.de)
+ * Aufgabe: Aufgabenblatt 2
+ * Main.java
+ *
+ * Application class for our list
+ *
+ * @author (Daniel.Sommerlig@haw-hamburg.de) &
+ *         (Lucasteffen.Nerlich@haw-hamburg.de)
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
